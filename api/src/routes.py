112fcec0eabfbf86a81523e40af5774a6a24b213
Bruno Bortolli <brunobortolli@outlook.com>
2020-11-18 23:12:32 -0300

    Alteração padrão de nome nas rotas
    Utilizar substantivos e no plural

--- api/src/routes.py
+++ api/src/routes.py
@@ -4,16 +4,16 @@ import src.group as group
 import src.auth as auth
 
 # User
-app.add_url_rule('/user/<string:id>', 'user-get', user.get, methods = ['GET'])
-app.add_url_rule('/user', 'user-insert', user.insert, methods = ['POST'])
-app.add_url_rule('/user', 'user-update', user.update, methods = ['PUT'])
-app.add_url_rule('/user/<string:id>/groups', 'user-groups-get', user.groups, methods = ['GET'])
+app.add_url_rule('/users/<string:id>', 'user-get', user.get, methods = ['GET'])
+app.add_url_rule('/users', 'user-insert', user.insert, methods = ['POST'])
+app.add_url_rule('/users', 'user-update', user.update, methods = ['PUT'])
+app.add_url_rule('/users/<string:id>/groups', 'user-groups-get', user.groups, methods = ['GET'])
 
 # Login
 app.add_url_rule('/login', 'login', auth.login, methods = ['POST'])
 
 # Grouups
-app.add_url_rule('/group/<string:id>', 'group-get', group.get, methods = ['GET'])
-app.add_url_rule('/group', 'group-insert', group.insert, methods = ['POST'])
-app.add_url_rule('/group/<string:group_id>/add/<string:user_id>', 'group-add-user', group.add_user, methods = ['GET'])
-app.add_url_rule('/group/<string:group_id>/join', 'group-join', group.join, methods = ['GET'])
+app.add_url_rule('/groups/<string:id>', 'group-get', group.get, methods = ['GET'])
+app.add_url_rule('/groups', 'group-insert', group.insert, methods = ['POST'])
+app.add_url_rule('/groups/<string:group_id>/add/<string:user_id>', 'group-add-user', group.add_user, methods = ['GET'])
+app.add_url_rule('/groups/<string:group_id>/join', 'group-join', group.join, methods = ['GET'])
